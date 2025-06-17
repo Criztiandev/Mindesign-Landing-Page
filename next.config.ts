0afbe68c0d67cdb6d013e7356896e95f49f602d6
Criztiandev <criztiandev@gmail.com>
2025-06-17 17:41:51 +0800

    feat: Update Next.js configuration to allow remote images from Unsplash and replace Image component with img tags in HomePage for improved compatibility.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   ignoreDuringBuilds: true,
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "images.unsplash.com",
+      port: "",
+      pathname: "/**",
+    },
+  ],
 };
 
 export default nextConfig;
